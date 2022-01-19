371419978f3929acc55fde56dcf13ae3be92aa48
Logan Keenan <d.logankeenan@gmail.com>
2022-01-19 07:22:50 -0600

    use dotenv for api url

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+extern crate dotenv;
+use dotenv::dotenv;
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+    dotenv().ok();
+
     let server = notes_demo::create();
 
     server.listen("127.0.0.1:8080").await?;
