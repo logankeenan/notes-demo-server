b530d01db586195640fb1e5c0bff499691ad2b17
Logan Keenan <d.logankeenan@gmail.com>
2022-01-30 15:31:32 -0600

    use AppState

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate dotenv;
 use dotenv::dotenv;
+use notes_demo::{AppState};
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     dotenv().ok();
 
-    let server = notes_demo::create();
+    let mut state = AppState::new();
+    state.environment.insert(String::from("API_ORIGIN"), dotenv::var("API_ORIGIN")?);
+
+    let server = notes_demo::create(state);
     let port = dotenv::var("PORT")?;
     server.listen(format!("0.0.0.0:{}", port)).await?;
     Ok(())
